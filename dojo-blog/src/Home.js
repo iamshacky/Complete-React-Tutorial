ff68cdb1abfff43e2a9f940ed921ba49a1f2a563
iamshacky <65470293+iamshacky@users.noreply.github.com>
2022-04-08 18:30:48 -0400

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,5 +1,5 @@
 /*
-Note 1: Button can't handle a click event function like normal JS does because the function would fire upon the page rendering.
+Note 1: Button can't handle the click event handleClickAgain('mario') function like normal JS does because the function would fire upon the page rendering.
 The solution is to wrap the click event function inside of an annonymous function. See clip of video by Net Ninja at
 https://youtu.be/0XSDAup85SA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&t=223
 */
