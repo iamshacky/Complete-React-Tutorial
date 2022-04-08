8ea6bde98ad768cb6651934f5c0ff01e42a8dc1f
iamshacky <65470293+iamshacky@users.noreply.github.com>
2022-04-08 18:21:09 -0400

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,3 +1,9 @@
+/*
+Note 1: Button can't handle a click event function like normal JS does because the function would fire upon the page rendering.
+The solution is to wrap the click event function inside of an annonymous function. See clip of video by Net Ninja at
+https://youtu.be/0XSDAup85SA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&t=223
+*/
+
 const Home = () => {
 
   const handleClick = (e) => {
@@ -12,7 +18,7 @@ const Home = () => {
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button> // Note 1 at top of page
     </div>
   );
 }
